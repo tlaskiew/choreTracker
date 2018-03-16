c7fd9ffda069bd6471fca7da35f430aa4d02434e
tlaskiew <troylaskiewicz@gmail.com>
2018-03-15 20:31:01 -0400

    Created users will be added to database. Sessions added with logout capabilities(Only for creating an account at this moment). Updated GUI to include a logout screen hub at activity main. Still need to implement adding sessions to user login.

--- app/src/main/java/cpsc430/choretracker/DisplayCreateAccount.java
+++ app/src/main/java/cpsc430/choretracker/DisplayCreateAccount.java
@@ -1,15 +1,23 @@
 package cpsc430.choretracker;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -30,46 +38,76 @@ public class DisplayCreateAccount extends AppCompatActivity {
         list.add("Parent");
         list.add("Child");
         addToList();
+
+        Button create = findViewById(R.id.buttonCreateAccount);
+        create.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                DatabaseReference myRef = database.getReference();
+
+                //Collect all data from user
+                EditText email = findViewById(R.id.textEmail);
+                final String userEmail = EncodeString(email.getText().toString());
+                EditText username = findViewById(R.id.textUsername);
+                final String user = username.getText().toString();
+                EditText pass = findViewById(R.id.textPassword);
+                final String password = pass.getText().toString();
+                Spinner role = findViewById(R.id.spinnerChooseRole);
+                final String chosenRole = role.getSelectedItem().toString();
+
+                myRef = myRef.child("Users").child(userEmail);
+
+                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild(user)){
+                            //Catch if an account with the username already exists
+                            TextView error = findViewById(R.id.createError);
+                            error.setText("Account Already Exists!");
+                        }else{
+                            Map<String, String> userData = new HashMap<>();
+                            userData.put("Username", user);
+                            userData.put("Email", userEmail);
+                            userData.put("Password", password);
+                            userData.put("Role", chosenRole);
+                            if (chosenRole.equals("Child")) {
+                                userData.put("Stars", "0");
+                            }
+                            //Add data to database
+                            dataSnapshot.getRef().child(user).setValue(userData);
+
+                            TextView error = findViewById(R.id.createError);
+                            if(chosenRole.equals("Parent") && !error.getText().toString().contains("Account Already Exists!")){
+                                //Go-To Parent View
+                                Intent intent = new Intent(v.getContext(), parentView.class);
+                                intent.putExtra("user", user);
+                                startActivity(intent);
+                            }else if(chosenRole.equals("Child") && !error.getText().toString().contains("Account Already Exists!")){
+                                //Go-To Child View
+                                Intent intent = new Intent(v.getContext(), childView.class);
+                                intent.putExtra("user", user);
+                                addLocal(user, userEmail, chosenRole);
+                                startActivity(intent);
+                            }
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
     }
 
-    //Create account after collecting user data
-    public void create(View v){
-        DatabaseReference myRef = database.getReference();
-
-        //Collect all data from user
-        EditText email = findViewById(R.id.textEmail);
-        String userEmail = email.getText().toString();
-        EditText username = findViewById(R.id.textUsername);
-        String user = username.getText().toString();
-        EditText pass = findViewById(R.id.textPassword);
-        String password = pass.getText().toString();
-        Spinner role = findViewById(R.id.spinnerChooseRole);
-        String chosenRole = role.getSelectedItem().toString();
-
-        Map<String, String> userData = new HashMap<>();
-        userEmail = EncodeString(userEmail);
-        userData.put("Username", user);
-        userData.put("Email", userEmail);
-        userData.put("Password", password);
-        userData.put("Role", chosenRole);
-        if(chosenRole.equals("Child")){
-            userData.put("Stars", "0");
-        }
-
-        myRef = myRef.child("Users").child(userEmail).child(user);
-
-        //Input value into database
-        myRef.setValue(userData);
-
-        if(chosenRole.equals("Parent")){
-            Intent intent = new Intent(this, parentView.class);
-            intent.putExtra("user", user);
-            startActivity(intent);
-        }else if(chosenRole.equals("Child")){
-            Intent intent = new Intent(this, childView.class);
-            intent.putExtra("user", user);
-            startActivity(intent);
-        }
+    //Adds a session for the user who logged in
+    public void addLocal(String user, String email, String role){
+        //Add user to signed in
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("Username", user);
+        editor.putString("Email", email);
+        editor.putString("Role", role);
+        editor.apply();
     }
 
     //Replaces . with , to allow the string to be put into database
--- app/src/main/java/cpsc430/choretracker/main.java
+++ app/src/main/java/cpsc430/choretracker/main.java
@@ -1,9 +1,12 @@
 package cpsc430.choretracker;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
@@ -12,7 +15,9 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class main extends AppCompatActivity {
     public final String EXTRA_MESSAGE = "MESSAGE";
-    public final String user = "";
+    private String user;
+    private String email;
+    private String role;
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
 
@@ -22,6 +27,68 @@ public class main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Search for already logged in user
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        user = preferences.getString("Username", "");
+        email = preferences.getString("Email", "").replace(".", ",");
+        role = preferences.getString("Role", "");
+
+        //User logged in currently is a parent
+        if(!user.equals("") && !email.equals("") && !role.equals("") && role.equals("Parent")){
+            loggedIn();
+        //User logged in currently a child
+        }else if(!user.equals("") && !email.equals("") && !role.equals("") && role.equals("Child")){
+            loggedIn();
+        }
+    }
+
+    //Clear local session data and then make visible needed items while making others invisible
+    public void logout(View v){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.apply();
+        Button view = findViewById(R.id.buttonView);
+        view.setVisibility(View.INVISIBLE);
+        Button logout = findViewById(R.id.buttonLogout);
+        logout.setVisibility(View.INVISIBLE);
+        EditText username = findViewById(R.id.username);
+        username.setVisibility(View.VISIBLE);
+        EditText pass = findViewById(R.id.password);
+        pass.setVisibility(View.VISIBLE);
+        Button login = findViewById(R.id.buttonLogin);
+        login.setVisibility(View.VISIBLE);
+        Button createAcc = findViewById(R.id.buttonCreateAccount);
+        createAcc.setVisibility(View.VISIBLE);
+    }
+
+    //Logged in, hide unneeded items
+    public void loggedIn(){
+        Button view = findViewById(R.id.buttonView);
+        view.setVisibility(View.VISIBLE);
+        Button logout = findViewById(R.id.buttonLogout);
+        logout.setVisibility(View.VISIBLE);
+        EditText username = findViewById(R.id.username);
+        username.setVisibility(View.INVISIBLE);
+        EditText pass = findViewById(R.id.password);
+        pass.setVisibility(View.INVISIBLE);
+        Button login = findViewById(R.id.buttonLogin);
+        login.setVisibility(View.INVISIBLE);
+        Button createAcc = findViewById(R.id.buttonCreateAccount);
+        createAcc.setVisibility(View.INVISIBLE);
+    }
+
+    //Sends user to appropriate page
+    public void view(View v){
+        if(role.equals("Child")) {
+            Intent intent = new Intent(this, childView.class);
+            intent.putExtra("user", user);
+            startActivity(intent);
+        }else if(role.equals("Parent")){
+            Intent intent = new Intent(this, parentView.class);
+            intent.putExtra("user", user);
+            startActivity(intent);
+        }
     }
 
     //Go-to createAccount view
